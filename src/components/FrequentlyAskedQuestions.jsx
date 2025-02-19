622ca34b037acc8ddabbdaca21f40d0a276ba29a
TheChirag356 <arorachirag12005@gmail.com>
2025-02-19 23:02:11 +0530

    fix:Fixed the buttons in faq section

--- src/components/FrequentlyAskedQuestions.jsx
+++ src/components/FrequentlyAskedQuestions.jsx
@@ -1,51 +1,26 @@
 import AnimatedTitle from "./AnimatedTitle.jsx";
+import { TiArrowSortedDown } from "react-icons/ti";
+import { useState } from "react";
 
-const QuestionCard = ({ question, answer }) => {
+const QuestionCard = ({ key, question, answer }) => {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <div
-      className="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 active"
-      id="hs-basic-with-title-and-arrow-stretched-heading-one"
-    >
+    <div className={`rounded-xl p-6 ${isActive ? "shadow-lg" : ""}`}>
       <button
-        className="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500"
-        aria-expanded="true"
-        aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one"
+        className="group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500"
+        onClick={() => setIsActive((prev) => !prev)}
       >
         {question}
-        <svg
-          className="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="m6 9 6 6 6-6" />
-        </svg>
-        <svg
-          className="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="m18 15-6-6-6 6" />
-        </svg>
+        <TiArrowSortedDown
+          className={`size-5 text-gray-600 group-hover:text-gray-500 ${
+            isActive ? "-rotate-90" : ""
+          }`}
+        />
       </button>
       <div
-        id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-        className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
-        role="region"
-        aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one"
+        className={`w-full overflow-hidden transition-[height] duration-300 ${
+          isActive ? "max-h-[1000px]" : "max-h-0"
+        }`}
       >
         <p className="text-gray-800">{answer}</p>
       </div>
@@ -63,7 +38,7 @@ const FrequentlyAskedQuestions = () => {
         <p className="font-['general'] text-sm uppercase md:text-[10px] small-text-container">
           Answers to the most frequently asked questions.
         </p>
-        <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+        <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-10">
           <AnimatedTitle
             title="Your questions, answered"
             containerClass="whitespace-nowrap !text-blue-200"
@@ -71,16 +46,15 @@ const FrequentlyAskedQuestions = () => {
         </div>
       </div>
 
-      <div className="max-w-2xl mx-auto">
+      <div className="max-w-3xl mx-auto">
         <div className="hs-accordion-group">
-          <QuestionCard
-            question="What is Nexathon and what makes it unique?"
-            answer="Nexathon is a unique AI based hackathon hosted by GTBIT Rajouri Garden and Nexus - The AI Society. It is unique because it is the first of its kind hackathon that focuses on AI and its applications in various fields."
-          />
-          <QuestionCard
-            question="What is Nexathon and what makes it unique?"
-            answer="Nexathon is a unique AI based hackathon hosted by GTBIT Rajouri Garden and Nexus - The AI Society. It is unique because it is the first of its kind hackathon that focuses on AI and its applications in various fields."
-          />
+          {qna.map((item, index) => (
+            <QuestionCard
+              key={index}
+              question={item.question}
+              answer={item.answer}
+            />
+          ))}
         </div>
       </div>
     </section>
@@ -88,3 +62,16 @@ const FrequentlyAskedQuestions = () => {
 };
 
 export default FrequentlyAskedQuestions;
+
+const qna = [
+  {
+    question: "What is Nexathon and what makes it unique?",
+    answer:
+      "Nexathon is a unique AI based hackathon hosted by GTBIT Rajouri Garden and Nexus - The AI Society. It is unique because it is the first of its kind hackathon that focuses on AI and its applications in various fields.",
+  },
+  {
+    question: "What is Nexathon and what makes it unique?",
+    answer:
+      "Nexathon is a unique AI based hackathon hosted by GTBIT Rajouri Garden and Nexus - The AI Society. It is unique because it is the first of its kind hackathon that focuses on AI and its applications in various fields.",
+  },
+];
