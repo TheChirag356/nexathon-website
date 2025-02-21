1a5ddf6a7e6fb4b9e4036e9fd24fb770cabcc2f6
TheChirag356 <arorachirag12005@gmail.com>
2025-02-21 19:11:33 +0530

    chore: Comment out dotenv import and console log for environment variable

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "dotenv/config";
+// import "dotenv/config";
+
+// console.log(process.env.BrandFetchClientId);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
