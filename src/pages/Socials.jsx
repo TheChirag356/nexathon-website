91eb3ffb69239a02f93ef176c3e82168ecdca95a
TheChirag356 <arorachirag12005@gmail.com>
2025-02-24 16:17:29 +0530

    feat: Revamp Socials page layout and enhance button styles for better user experience

--- src/pages/Socials.jsx
+++ src/pages/Socials.jsx
@@ -6,40 +6,45 @@ const Button = ({ leftLogo, title, containerClass, to }) => {
 
   return (
     <button
-      className="cursor-pointer relative inline-flex h-12 w-full items-center justify-center rounded-md bg-white px-6 font-['circular-web'] font-medium text-gray-950 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50"
+      className="relative inline-flex h-14 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50"
       onClick={() => window.open(to, "_blank")}
     >
-      <div className="absolute -inset-0.5 -z-10 rounded-lg bg-gradient-to-b from-[#c7d2fe] to-[#8678f9] opacity-75 blur" />
-      {title}
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-gray-950 px-8 py-1 text-sm font-medium text-gray-50 backdrop-blur-3xl">
+        {title}
+      </span>
     </button>
   );
 };
 
 const Socials = () => {
   return (
-    <div className=" h-dvh w-dvw text-white">
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-col items-center my-16 md:my-24 gap-8 w-xl">
-          <div className="flex flex-col gap-4 size-full items-center">
-            <img
-              src="img/nexus_logo.jpg"
-              alt=""
-              className="w-[5vw] rounded-[50%]"
-            />
-            <h1 className="font-['circular-web'] font-semibold text-3xl">
-              Nexus - The AI Society
-            </h1>
-          </div>
-          <div className="flex flex-col gap-4 size-full">
-            <Button logo={<FaWhatsapp />} title="Whatsapp Group" />
-            <Button logo={<FaDiscord />} title="Discord Server" />
-            <Button
-              logo={<FaInstagram />}
-              title="Nexus Instagram"
-              to="https://www.instagram.com/nexus_gtbit/"
-            />
-            <Button logo={<FaInstagram />} title="Nexathon Instagram" />
+    <div className=" h-dvh w-dvw">
+      <div class="relative h-full w-full bg-slate-950">
+        <div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center my-16 md:my-24 gap-8 w-xl">
+            <div className="flex flex-col gap-4 size-full items-center">
+              <img
+                src="img/nexus_logo.jpg"
+                alt=""
+                className="w-[5vw] rounded-[50%]"
+              />
+              <h1 className="font-['circular-web'] font-semibold text-3xl text-white">
+                Nexus - The AI Society
+              </h1>
+            </div>
+            <div className="flex flex-col gap-4 size-full">
+              <Button logo={<FaWhatsapp />} title="Whatsapp Group" />
+              <Button logo={<FaDiscord />} title="Discord Server" />
+              <Button
+                logo={<FaInstagram />}
+                title="Nexus Instagram"
+                to="https://www.instagram.com/nexus_gtbit/"
+              />
+              <Button logo={<FaInstagram />} title="Nexathon Instagram" />
+            </div>
           </div>
         </div>
       </div>
