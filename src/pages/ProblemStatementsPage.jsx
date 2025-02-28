9e0e01ae7486b4d63713c1d8085da67068c90840
TheChirag356 <arorachirag12005@gmail.com>
2025-02-28 19:29:18 +0530

    feat: Redesign ProblemStatementsPage with new layout and structure

--- src/pages/ProblemStatementsPage.jsx
+++ src/pages/ProblemStatementsPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const ProblemStatementsPage = () => {
   return (
-    <div>ProblemStatementsPage</div>
-  )
-}
+    <div>
+      <div className="container">
+        <nav>
+          <p id="logo">Voxel</p>
+          <button>Download now</button>
+        </nav>
 
-export default ProblemStatementsPage
+        <section className="intro"></section>
+        <section className="steps">
+          <div className="step-counter">
+            <div className="counter-title">
+              <h1>Steps</h1>
+            </div>
+            <div className="count">
+              <div className="count">
+                <h1>01</h1>
+                <h1>02</h1>
+                <h1>03</h1>
+                <h1>04</h1>
+                <h1>05</h1>
+              </div>
+            </div>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <div className="card-img">
+                <img src="" alt="" />
+              </div>
+              <div className="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, numquam?</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="outro"></section>
+      </div>
+    </div>
+  );
+};
+
+export default ProblemStatementsPage;
